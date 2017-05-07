2890589f4ae55f7c0250232ba96964309983dae3
Ori Bar-ilan <ori303@gmail.com>
2017-05-08 00:39:01 +0300

    Added bs to diagnosis, need to test more examples and add epsilon

--- Diagnoser/Barinel.py
+++ Diagnoser/Barinel.py
@@ -15,26 +15,42 @@ class Barinel:
 
     def __init__(self):
         self.M_matrix = []
+        self.b_matrix = []
         self.e_vector = []
         self.diagnoses = []
 
 
-    def set_matrix_error(self,M,e):
+    def set_matrix_bMatrix_error(self, M, bMatrix, e):
         self.M_matrix = M
+        self.b_matrix = bMatrix
         self.e_vector = e
 
     def generate_probs(self):
         new_diagnoses = []
         probs_sum = 0.0
         for diag in self.diagnoses:
-            dk = math.pow(prior_p,len(diag.get_diag())) #assuming same prior prob. for every component.
-            tf = TF.TF(self.M_matrix,self.e_vector,diag.get_diag())
-            e_dk = tf.maximize()
-            diag.probability=e_dk * dk #temporary probability
+            # Barinel's:
+            # dk = math.pow(prior_p,len(diag.get_diag())) #assuming same prior prob. for every component.
+            # tf = TF.TF(self.M_matrix,self.e_vector,diag.get_diag())
+            # e_dk = tf.maximize()
+            # diag.probability = e_dk * dk #temporary probability
+            # Ori's:
+            total_diag_prob = 1
+            for comp in diag.diagnosis:
+                comp_prob_acc = 1
+                for i, observation in enumerate(self.M_matrix):
+                    if observation[comp] == 0:  # component didn't take part in test
+                        continue
+                    if self.e_vector[i] == 0:  # test didn't fail
+                        comp_prob_acc *= 1 - self.b_matrix[i][comp]
+                    else:  # test failed
+                        comp_prob_acc *= self.b_matrix[i][comp]
+                total_diag_prob *= comp_prob_acc
+            diag.probability = total_diag_prob
             probs_sum += diag.probability
-        for diag in self.diagnoses:
+        for diag in self.diagnoses: #normalizing
             temp_prob = diag.get_prob() / probs_sum
-            diag.probability=temp_prob
+            diag.probability = temp_prob
             new_diagnoses.append(diag)
         self.diagnoses = new_diagnoses
 
@@ -55,12 +71,17 @@ class Barinel:
 def load_file_with_header( file):
     with open(file,"r") as f:
         lines = list(csv.reader(f))
-        comps_num=len(lines[0])-1
+        comps_num = int((len(lines[0])-1)/2)
+        error_vec_col_idx = comps_num * 2
         tests = lines[0:]
-        erorr_vector = [int(t[comps_num]) for t in tests]
-        Matrix = [[int(float(y)) for y in x[:comps_num]] for x in tests]
+        erorr_vector = [int(t[error_vec_col_idx]) for t in tests]
+        Matrix = [0 for t in tests]
+        bMatrix = [0 for t in tests]
+        for idx, t in enumerate(tests):
+            Matrix[idx] = [int(elm) for elm in t[:error_vec_col_idx:2]] #a matrix
+            bMatrix[idx] = [int(elm) for elm in t[1:error_vec_col_idx:2]] #b matrix
         ans = Barinel()
-        ans.set_matrix_error(Matrix,erorr_vector)
+        ans.set_matrix_bMatrix_error(Matrix, bMatrix, erorr_vector)
         return ans
 
 
@@ -75,8 +96,9 @@ def test(matrix_file, out_file):
 if __name__=="__main__":
     matrix_file = os.path.join(os.path.dirname(__file__), "planning_example\\{0}.csv")
     out_file = os.path.join(os.path.dirname(__file__), "planning_example\\{0}.txt")
-    test(matrix_file.format('all'), out_file.format('all'))
-    test(matrix_file.format('s0'), out_file.format('s0'))
-    test(matrix_file.format('choose_worse'), out_file.format('choose_worse'))
-    test(matrix_file.format('choose_better'), out_file.format('choose_better'))
-    test(matrix_file.format('choose_best'), out_file.format('choose_best'))
+    test(matrix_file.format('withb'), out_file.format('withb'))
+    # test(matrix_file.format('all'), out_file.format('all'))
+    # test(matrix_file.format('s0'), out_file.format('s0'))
+    # test(matrix_file.format('choose_worse'), out_file.format('choose_worse'))
+    # test(matrix_file.format('choose_better'), out_file.format('choose_better'))
+    # test(matrix_file.format('choose_best'), out_file.format('choose_best'))
